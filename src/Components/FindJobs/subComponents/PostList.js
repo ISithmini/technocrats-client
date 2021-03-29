8872a14256c24a30bf4cc9e0809fcd81516c636f
nipunaluthge <nipunaluthge4815@gmail.com>
2021-03-29 13:05:20 +0530

    Started working on jpb preview component

--- src/Components/FindJobs/subComponents/PostList.js
+++ src/Components/FindJobs/subComponents/PostList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import JobPreviewComponent from '../../JobComponents/JobPreviewTileComponent/JobPreviewComponent'
 import SkeletonJobPost from '../../Skeleton/SkeletonJobPost'
 
 const PostList = () => {
   return (
     <div>
+      <JobPreviewComponent/>
       {
         [1, 2, 3, 5, 6].map(post => {
           return (
