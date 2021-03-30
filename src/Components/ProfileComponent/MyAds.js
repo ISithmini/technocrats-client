899824e7ef3cbed1525502f332e304a684c84677
Sanjana Ganegoda <sanjanasithira29@gmail.com>
2021-03-30 01:16:30 -0700

    initial commit of profile

---
+++ src/Components/ProfileComponent/MyAds.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './MyAds.scss'
+
+export default function MyAds() {
+    return (
+        <div className="myads-container">
+        <h3 className="myads-info-title">My Ads</h3>
+        </div>
+    )
+}
---
+++ src/Components/ProfileComponent/MyChats.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './MyChats.scss'
+
+export default function MyChats() {
+    return (
+        <div className="mychats-container">
+        <h3 className="mychats-info-title">My Chats</h3>
+        </div>
+    )
+}
